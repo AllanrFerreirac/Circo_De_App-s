a1ca718af843742566cf89707b9a0c46ead144c1
Allan Ferreira <89280714+AllanrFerreirac@users.noreply.github.com>
2021-11-18 22:22:37 -0300

    Add files via upload

---
+++ FrmBuscaCEP.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace CircoDeApp
+{
+    partial class FrmBuscaCEP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNovoCEP = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.lblNovoEndereco = new System.Windows.Forms.Label();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.lblNovoComplemento = new System.Windows.Forms.Label();
+            this.lblComplemento = new System.Windows.Forms.Label();
+            this.lblNovoBairro = new System.Windows.Forms.Label();
+            this.lblBairro = new System.Windows.Forms.Label();
+            this.lblNovaCidade = new System.Windows.Forms.Label();
+            this.lblCidade = new System.Windows.Forms.Label();
+            this.lblUF = new System.Windows.Forms.Label();
+            this.lblNovaUF = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNovoCEP
+            // 
+            this.lblNovoCEP.AutoSize = true;
+            this.lblNovoCEP.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNovoCEP.Location = new System.Drawing.Point(8, 27);
+            this.lblNovoCEP.Name = "lblNovoCEP";
+            this.lblNovoCEP.Size = new System.Drawing.Size(145, 24);
+            this.lblNovoCEP.TabIndex = 0;
+            this.lblNovoCEP.Text = "Digite um CEP";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(366, 54);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(109, 29);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(497, 54);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(109, 29);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCEP.Location = new System.Drawing.Point(12, 54);
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(324, 29);
+            this.txtCEP.TabIndex = 4;
+            // 
+            // lblNovoEndereco
+            // 
+            this.lblNovoEndereco.AutoSize = true;
+            this.lblNovoEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNovoEndereco.Location = new System.Drawing.Point(8, 96);
+            this.lblNovoEndereco.Name = "lblNovoEndereco";
+            this.lblNovoEndereco.Size = new System.Drawing.Size(102, 24);
+            this.lblNovoEndereco.TabIndex = 5;
+            this.lblNovoEndereco.Text = "Endereço";
+            // 
+            // lblEndereco
+            // 
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEndereco.Location = new System.Drawing.Point(8, 148);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(0, 24);
+            this.lblEndereco.TabIndex = 6;
+            // 
+            // lblNovoComplemento
+            // 
+            this.lblNovoComplemento.AutoSize = true;
+            this.lblNovoComplemento.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNovoComplemento.Location = new System.Drawing.Point(493, 96);
+            this.lblNovoComplemento.Name = "lblNovoComplemento";
+            this.lblNovoComplemento.Size = new System.Drawing.Size(140, 24);
+            this.lblNovoComplemento.TabIndex = 7;
+            this.lblNovoComplemento.Text = "Complemento";
+            // 
+            // lblComplemento
+            // 
+            this.lblComplemento.AutoSize = true;
+            this.lblComplemento.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComplemento.Location = new System.Drawing.Point(475, 148);
+            this.lblComplemento.Name = "lblComplemento";
+            this.lblComplemento.Size = new System.Drawing.Size(0, 24);
+            this.lblComplemento.TabIndex = 8;
+            // 
+            // lblNovoBairro
+            // 
+            this.lblNovoBairro.AutoSize = true;
+            this.lblNovoBairro.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNovoBairro.Location = new System.Drawing.Point(12, 201);
+            this.lblNovoBairro.Name = "lblNovoBairro";
+            this.lblNovoBairro.Size = new System.Drawing.Size(65, 24);
+            this.lblNovoBairro.TabIndex = 9;
+            this.lblNovoBairro.Text = "Bairro";
+            // 
+            // lblBairro
+            // 
+            this.lblBairro.AutoSize = true;
+            this.lblBairro.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBairro.Location = new System.Drawing.Point(12, 250);
+            this.lblBairro.Name = "lblBairro";
+            this.lblBairro.Size = new System.Drawing.Size(0, 24);
+            this.lblBairro.TabIndex = 10;
+            // 
+            // lblNovaCidade
+            // 
+            this.lblNovaCidade.AutoSize = true;
+            this.lblNovaCidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNovaCidade.Location = new System.Drawing.Point(8, 300);
+            this.lblNovaCidade.Name = "lblNovaCidade";
+            this.lblNovaCidade.Size = new System.Drawing.Size(76, 24);
+            this.lblNovaCidade.TabIndex = 11;
+            this.lblNovaCidade.Text = "Cidade";
+            // 
+            // lblCidade
+            // 
+            this.lblCidade.AutoSize = true;
+            this.lblCidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCidade.Location = new System.Drawing.Point(12, 363);
+            this.lblCidade.Name = "lblCidade";
+            this.lblCidade.Size = new System.Drawing.Size(0, 24);
+            this.lblCidade.TabIndex = 12;
+            // 
+            // lblUF
+            // 
+            this.lblUF.AutoSize = true;
+            this.lblUF.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUF.Location = new System.Drawing.Point(475, 363);
+            this.lblUF.Name = "lblUF";
+            this.lblUF.Size = new System.Drawing.Size(0, 24);
+            this.lblUF.TabIndex = 14;
+            // 
+            // lblNovaUF
+            // 
+            this.lblNovaUF.AutoSize = true;
+            this.lblNovaUF.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNovaUF.Location = new System.Drawing.Point(493, 300);
+            this.lblNovaUF.Name = "lblNovaUF";
+            this.lblNovaUF.Size = new System.Drawing.Size(37, 24);
+            this.lblNovaUF.TabIndex = 13;
+            this.lblNovaUF.Text = "UF";
+            // 
+            // FrmBuscaCEP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(661, 394);
+            this.Controls.Add(this.lblUF);
+            this.Controls.Add(this.lblNovaUF);
+            this.Controls.Add(this.lblCidade);
+            this.Controls.Add(this.lblNovaCidade);
+            this.Controls.Add(this.lblBairro);
+            this.Controls.Add(this.lblNovoBairro);
+            this.Controls.Add(this.lblComplemento);
+            this.Controls.Add(this.lblNovoComplemento);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.lblNovoEndereco);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.lblNovoCEP);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmBuscaCEP";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Busca CEP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNovoCEP;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.TextBox txtCEP;
+        private System.Windows.Forms.Label lblNovoEndereco;
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.Label lblNovoComplemento;
+        private System.Windows.Forms.Label lblComplemento;
+        private System.Windows.Forms.Label lblNovoBairro;
+        private System.Windows.Forms.Label lblBairro;
+        private System.Windows.Forms.Label lblNovaCidade;
+        private System.Windows.Forms.Label lblCidade;
+        private System.Windows.Forms.Label lblUF;
+        private System.Windows.Forms.Label lblNovaUF;
+    }
+}
---
+++ FrmPrincipal.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace CircoDeApp
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBuscaCEP = new System.Windows.Forms.Label();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.pbxCEP = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCEP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblBuscaCEP
+            // 
+            this.lblBuscaCEP.AutoSize = true;
+            this.lblBuscaCEP.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBuscaCEP.Location = new System.Drawing.Point(46, 185);
+            this.lblBuscaCEP.Name = "lblBuscaCEP";
+            this.lblBuscaCEP.Size = new System.Drawing.Size(114, 24);
+            this.lblBuscaCEP.TabIndex = 0;
+            this.lblBuscaCEP.Text = "Busca CEP";
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.DarkGray;
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(612, 545);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(121, 55);
+            this.btnSair.TabIndex = 1;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // pbxCEP
+            // 
+            this.pbxCEP.Image = global::CircoDeApp.Properties.Resources.cep;
+            this.pbxCEP.Location = new System.Drawing.Point(24, 32);
+            this.pbxCEP.Name = "pbxCEP";
+            this.pbxCEP.Size = new System.Drawing.Size(157, 150);
+            this.pbxCEP.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxCEP.TabIndex = 2;
+            this.pbxCEP.TabStop = false;
+            this.pbxCEP.Click += new System.EventHandler(this.pbxCEP_Click);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(757, 612);
+            this.Controls.Add(this.pbxCEP);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.lblBuscaCEP);
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Circo de App\'s";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCEP)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBuscaCEP;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.PictureBox pbxCEP;
+    }
+}
+
